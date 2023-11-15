697a4be6ea691d2e9e8aa13c4ee4f957f2fb88c1
shafiaansar <shafiaansar142@gmail.com>
2023-11-15 11:47:56 +0500

    Layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@
 import {twMerge} from 'tailwind-merge'
 import { useRouter } from "next/navigation"
 import {RxCaretLeft, RxCaretRight} from 'react-icons/rx'
+import { HiHome } from 'react-icons/hi'
+import { BiSearch } from 'react-icons/bi'
+import Button from './Button'
 interface HeaderProps{
   children:React.ReactNode
   className?:string
@@ -20,7 +23,30 @@ const Header:React.FC<HeaderProps> = ({children,className}) => {
             <RxCaretRight className='text-white' size={35}/>
           </button>
         </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className='rounded-full bg-white flex items-center justify-center hover:opacity-75 transition p-2'>
+            <HiHome className='text-black' size={20}/>
+          </button>
+          <button className='rounded-full bg-white flex items-center justify-center hover:opacity-75 transition p-2'>
+            <BiSearch className='text-black' size={20}/>
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4 ">
+          <>
+          <div className="">
+            <Button onClick={()=>{}}  className='bg-transparent text-neutral-300 font-medium'>
+              Sign up
+            </Button>
+          </div>
+          <div className="">
+            <Button onClick={()=>{}} className='bg-white px-6 py-2'>
+              Login
+            </Button>
+          </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   )
 }
