dddbd5cd213b5c7a84e432deafc46ccc9b6bfeb7
shafiaansar <shafiaansar142@gmail.com>
2023-11-20 11:34:49 +0500

    next and prev song func

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,21 +1,50 @@
 'use client'
-
+import {useState} from 'react'
 import { Song } from "@/types"
 import MediaItem from "./MediaItem"
 import LikeButton from "./LikeButton"
 import{BsPauseFill,BsPlayFill} from 'react-icons/bs'
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai"
+import {HiSpeakerWave, HiSpeakerXMark}from 'react-icons/hi2'
+import Slider from "./Slider"
+import usePlayer from "@/hooks/usePlayer"
  interface PlayerContentProps{
     song:Song
     songUrl:string
  }
 const PlayerContent:React.FC<PlayerContentProps> = ({song,songUrl}) => {
-    const Icon = true? BsPauseFill : BsPlayFill
+    const player = usePlayer()
+    const [volume,setVolume] = useState(1)
+    const [isPlaying,setIsPlaying] = useState(false)
+    const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave
+    const Icon = isPlaying ? BsPauseFill : BsPlayFill
+    const onPlayNext = ()=>{
+        if(player.ids.length===0){
+            return
+        }
+        const currentIndex = player.ids.findIndex((id)=> id === player.activeId)
+        const nextSong = player.ids[currentIndex+1]
+        if(!nextSong){
+            return player.setId(player.ids[0])
+        }
+        player.setId(nextSong)
+    }
+    const onPlayBack = ()=>{
+        if(player.ids.length === 0 ){
+            return
+        }
+        const currentIndex = player.ids.findIndex((id)=> id === player.activeId)
+        const prevSong = player.ids[currentIndex - 1]
+        if(!prevSong){
+            return player.setId(player.ids[player.ids.length - 1 ])
+        }
+        player.setId(prevSong)
+    }
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
         <div className="flex w-full justify-start ">
             <div className="flex items-center gap-x-4">
-                <MediaItem data={song}/>
+                <MediaItem data={song} onClick={()=>{}}/>
                 <LikeButton songId={song.id}/>
             </div>
         </div>
@@ -25,11 +54,17 @@ const PlayerContent:React.FC<PlayerContentProps> = ({song,songUrl}) => {
             </div>
         </div>
         <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
-            <AiFillStepBackward size={30 } className="cursor-pointer text-neutral-400 hover:text-white transition" onClick={()=>{}}/>
+            <AiFillStepBackward size={30 } className="cursor-pointer text-neutral-400 hover:text-white transition" onClick={onPlayBack}/>
             <div onClick={()=>{}} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer ">
                 <Icon size={30} className="text-black"/>
             </div>
-            <AiFillStepForward onClick={()=>{}} size={30 } className="cursor-pointer text-neutral-400 hover:text-white transition-black"/>
+            <AiFillStepForward onClick={onPlayNext} size={30 } className="cursor-pointer text-neutral-400 hover:text-white transition-black"/>
+        </div>
+        <div className="hidden md:flex w-full justify-end pr-2">
+            <div className="flex items-center gap-x-2 w-[120px]">
+                <VolumeIcon onClick={()=>{}} size={34} className="cursor-pointer"/>
+                <Slider/>
+            </div>
         </div>
         </div>
   )
